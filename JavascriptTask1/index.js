7f80c54c43ea4311c7f5547df433ee8453935bf7
pranjul bansal <pranjulbansal672@gmail.com>
2025-07-08 22:47:38 +0530

    Task of Login and Register with Javascript Completed

---
+++ JavascriptTask1/index.js
@@ -0,0 +1,18 @@
+function submitLogin() {
+  const email = document.getElementById("email").value;
+  console.log(email);
+  const pass = document.getElementById("pass").value;
+  console.log(pass);
+  const rem = document.getElementById("rem").value;
+  console.log(rem);
+}
+function submitRegister() {
+  const name = document.getElementById("name").value;
+  console.log(name);
+  const email2 = document.getElementById("email2").value;
+  console.log(email2);
+  const pass2 = document.getElementById("pass2").value;
+  console.log(pass2);
+  const pass3 = document.getElementById("pass3").value;
+  console.log(pass3);
+}
